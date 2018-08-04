f54206ba607b8822bd7485be4620f8b619c0775f
Digambar <digambarb@users.noreply.github.com>
2018-08-05 01:22:51 +0530

    added overloaded function call for addition
    added overloaded function call for addition with doubles as argument

--- Addition.c
+++ Addition.c
@@ -4,3 +4,10 @@ int addition(int firstNo , int secondNo)
   return ((firstNo) + (secondNo));
   
  }
+
+double addition(double firstNo , double secondNo)
+{
+
+  return ((firstNo) + (secondNo));
+  
+ }
